db621a10781bfe987113bbd2eb1e94787c848dee
captainTigerYT <raunakmanna43@gmail.com>
2025-02-03 23:20:01 +0530

    updated prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+// @ts-nocheck
+import { PrismaClient } from "../../prisma/generated/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+let prisma: PrismaClient;
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
