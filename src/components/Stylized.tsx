1cbaded6c1d98db65d528fe3cd462c89a4a062e7
Raunak M <raunakmanna43@gmail.com>
2025-12-09 10:56:53 +0530

    feat: add spline follower component and update dependencies
    - Added @splinetool/react-spline and @splinetool/runtime to package.json.
    - Updated Next.js version to ^15.3.5.
    - Refactored Stylized component for better readability and added Follower component.
    - Created Follower component to render a Spline scene.

--- src/components/Stylized.tsx
+++ src/components/Stylized.tsx
@@ -3,46 +3,50 @@ import HeroText from "@/components/HeroText";
 import TextReveal from "@/components/ui/text-reveal";
 import { Mouse } from "lucide-react";
 import { VelocityScroll } from "@/components/ui/scroll-based-velocity";
-import { codeFont, highlightFont} from "@/app/fonts";
+import { codeFont, highlightFont } from "@/app/fonts";
+// import Follower from "./spline/follower";
 
 const skills =
-  "Web Android NextJS ReactJS CSS HTML5 NodeJS Prisma Postgres UI/UX Tailwind Typescript Javascript Java C C++ Python ML DSA Firebase DJango ".split(
-    " "
-  );
+    "Web Android NextJS ReactJS CSS HTML5 NodeJS Prisma Postgres UI/UX Tailwind Typescript Javascript Java C C++ Python ML DSA Firebase DJango ".split(
+        " "
+    );
 
 const Stylized = () => {
-  return (
-    <div>
-      <div className="w-screen h-[90vh] flex flex-col items-center justify-between">
-        <div className="text-left flex-grow flex flex-col items-center justify-center">
-          <span className={`text-3xl pl-2 ${codeFont.className}`}>Hi from</span>
-          <HeroText text="Raunak" className=" p-0" />
-        </div>
-        <div className="pb-1 animate-jump">
-          <Mouse />
-        </div>
-      </div>
-      <div className="z-10 flex min-h-64 justify-start w-full">
-        <TextReveal text="I am a web developer from India" />
-      </div>
-      <div className="z-10 flex min-h-64 justify-end w-full">
-        <TextReveal text="What i do defines me" />
-      </div>
+    return (
+        <div>
+            <div className="w-screen h-[90vh] flex flex-col items-center justify-between">
+                <div className="text-left flex-grow flex flex-col items-center justify-center">
+                    <span className={`text-3xl pl-2 ${codeFont.className}`}>
+                        Hi from
+                    </span>
+                    <HeroText text="Raunak" className=" p-0" />
+                </div>
+                <div className="pb-1 animate-jump">
+                    <Mouse />
+                </div>
+                {/* <Follower /> */}
+            </div>
+            <div className="z-10 flex min-h-64 justify-start w-full">
+                <TextReveal text="I am a web developer from India" />
+            </div>
+            <div className="z-10 flex min-h-64 justify-end w-full">
+                <TextReveal text="What i do defines me" />
+            </div>
 
-      <div className="opacity-50">
-        <VelocityScroll
-          text={skills.sort(() => Math.random() - 0.5).join(" ")}
-          default_velocity={1}
-          className={`font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem] ${highlightFont.className}`}
-        />
-        <VelocityScroll
-          text={skills.sort(() => Math.random() - 0.5).join(" ")}
-          default_velocity={1}
-          className={`font-display text-center text-4xl italic font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem] ${codeFont.className}`}
-        />
-      </div>
-    </div>
-  );
+            <div className="opacity-50">
+                <VelocityScroll
+                    text={skills.sort(() => Math.random() - 0.5).join(" ")}
+                    default_velocity={1}
+                    className={`font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem] ${highlightFont.className}`}
+                />
+                <VelocityScroll
+                    text={skills.sort(() => Math.random() - 0.5).join(" ")}
+                    default_velocity={1}
+                    className={`font-display text-center text-4xl italic font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem] ${codeFont.className}`}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Stylized;
---
+++ src/components/spline/Follower.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Follower() {
+    return (
+        <main>
+            <Spline scene="https://prod.spline.design/v8c-eNZHKWr1fXPF/scene.splinecode" />
+        </main>
+    );
+}
